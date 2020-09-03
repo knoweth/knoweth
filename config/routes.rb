ca90ef85661f6eb021ed48f46e62f76b6167a2c0
Kevin Liu <kevin@kliu.io>
2020-09-03 19:17:23 -0400

    Add note create/destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :documents
+  resources :documents do
+    resources :notes, only: [:create, :destroy, :update]
+  end
 
   root "welcome#index"
 end
