6b6567e98fec08df2207306a0b98a8ef7e817618
Kevin Liu <kevin@kliu.io>
2020-09-04 10:03:41 -0400

    Enable action text for note

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
+// Action Text support
+require("trix");
+require("@rails/actiontext");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,5 +18,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import 'jquery'
-import 'popper.js'
+import "jquery";
+import "popper.js";
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :document
+
+  has_rich_text :left
+  has_rich_text :right
 end
