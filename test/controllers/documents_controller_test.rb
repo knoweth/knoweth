6027d392f295d6565bfafc8e6d4834f7ae412a39
Kevin Liu <kevin@kliu.io>
2020-09-06 23:50:22 -0400

    Add tests to document controller

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class DocumentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test "authenticated users can see documents" do
+    sign_in users(:alex)
+    get documents_path
+    assert_response :success
+  end
+
+  test "unauthenticated users cannot see documents" do
+    get documents_path
+    assert_response :redirect # (to login)
+  end
 end
