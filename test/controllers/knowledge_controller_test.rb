ffd0db5c8ba9c0538b973dbd63f4c0d7d318dfa6
Kevin Liu <kevin@kliu.io>
2020-09-13 21:52:35 -0400

    Fix rails tests

--- test/controllers/knowledge_controller_test.rb
+++ test/controllers/knowledge_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class KnowledgeControllerTest < ActionDispatch::IntegrationTest
-  test "should get upsert" do
-    get review_upsert_url
+  test "successfully upserts new knowledge" do
+    put("/knowledge/upsert", params: { knowledges: [{
+      card_id: "bob",
+      document_id: documents(:one).id,
+      ease_factor: 1,
+      interval_s: 1,
+      learning_step: 0,
+      repetitions: 0,
+      last_review: Time.now
+    }] })
     assert_response :success
   end
 
