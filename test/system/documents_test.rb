afd9ec123c1faaf1681a07a71b029ef9fefb9378
Kevin Liu <kevin@kliu.io>
2020-11-27 15:56:24 -0500

    Add system test for renaming document

--- test/system/documents_test.rb
+++ test/system/documents_test.rb
@@ -10,13 +10,30 @@ class DocumentsTest < ApplicationSystemTestCase
 
   test "creates document" do
     sign_in_as(users(:alex))
+    create_document
+  end
 
+  test "renames document" do
+    sign_in_as(users(:alex))
+    create_document
+    click_on 'Rename'
+
+    fill_in 'document_title', with: 'Test2'
+    sleep(inspection_time=1) # magic required ...
+    click_on 'Update Document'
+
+    assert_text 'Test2'
+    assert_text 'All changes saved' # Good save
+  end
+
+  private def create_document
     visit documents_url
     click_on 'Create new document'
 
     fill_in 'document_title', with: 'Test'
     click_on 'Save Document'
 
+    assert_text 'Test'
     assert_text 'All changes saved' # Good save
   end
 end
