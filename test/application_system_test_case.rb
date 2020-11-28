cdb11d8c924b391b51c9be01563c8aa8bb84609b
Kevin Liu <kevin@kliu.io>
2020-11-28 12:29:10 -0500

    Add headless args to fix actions

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400], options: {args: %w(headless disable-gpu no-sandbox disable-dev-shm-usage)}
 
   private def sign_in_as(user)
     visit new_user_session_url
